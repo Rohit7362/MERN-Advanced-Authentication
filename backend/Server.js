ccd4ab6e95d885e75dd9b5ea9bade6370f577a30
Rohit7362 <rd736291@gmail.com>
2026-01-14 00:14:35 +0530

    Fix wildcard route for Express on Linux

--- backend/Server.js
+++ backend/Server.js
@@ -20,10 +20,10 @@ app.use(cookieParser()); //allows us to parse incoming cookies
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 	});
 }
 
